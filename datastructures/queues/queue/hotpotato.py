9f6ada7e9027f280309f13b8c7ff45fc4d9bb117
munnu <moniqueblake4@gmail.com>
2016-07-12 11:30:37 -0700

    practicing, getting mah edumacation: queues, linkedlists

--- datastructures/queues/queue/hotpotato.py
+++ datastructures/queues/queue/hotpotato.py
@@ -1,17 +1,17 @@
 from queue import Queue
 
-def hotpotato(namelist, num):
+def hotPotato(namelist, num):
     simqueue = Queue()
     for name in namelist:
         simqueue.enqueue(name)
 
     while simqueue.size() > 1:
-        print simqueue.size()
         for i in range(num):
+            print simqueue.items
             simqueue.enqueue(simqueue.dequeue())
 
         simqueue.dequeue()
 
     return simqueue.dequeue()
 
-print hotpotato(["Bill","David","Susan","Jane","Kent","Brad"],7)
+print hotPotato(["Bill", "David", "Susan", "Jane", "Kent", "Brad"], 7)
