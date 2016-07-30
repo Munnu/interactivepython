7fb58223266df5096fa1503e3b7704064ee79a95
munnu <moniqueblake4@gmail.com>
2016-07-30 15:40:31 -0700

    fixed bugs, added way to actually use code at bottom

--- datastructures/graphs/graph.py
+++ datastructures/graphs/graph.py
@@ -24,7 +24,7 @@ class Vertex:
 
 class Graph:
     """ Holds master list of all vertices """
-    def __init__(self, key):
+    def __init__(self):
         self.vertList = {}
         self.numVertices = 0
     
@@ -55,3 +55,21 @@ class Graph:
 
     def __iter__(self):
         return iter(self.vertList.values())
+
+# example of how this thing works
+g = Graph()
+for i in range(6):
+    g.addVertex(i)
+g.vertList
+g.addEdge(0,1,5)
+g.addEdge(0,5,2)
+g.addEdge(1,2,4)
+g.addEdge(2,3,9)
+g.addEdge(3,4,7)
+g.addEdge(3,5,3)
+g.addEdge(4,0,1)
+g.addEdge(5,4,8)
+g.addEdge(5,2,1)
+for vertex in g:
+    for weight in vertex.getConnections():
+        print "(%s, %s)" % (vertex.getId(), weight.getId())
